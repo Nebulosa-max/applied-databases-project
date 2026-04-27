a82f106d4011b3939eecf7acf55ea2de8a84471e
Sophia Gotch <sophiegotch@users.noreply.github.com>
2026-04-27 22:06:10 +0100

    Add main menu structure

---
+++ main.py
@@ -0,0 +1,31 @@
+from utils.menu import print_menu
+
+
+def main():
+    while True:
+        print_menu()
+        choice = input("Choice: ").strip()
+
+        if choice == "1":
+            print("Option 1 selected")
+        elif choice == "2":
+            print("Option 2 selected")
+        elif choice == "3":
+            print("Option 3 selected")
+        elif choice == "4":
+            print("Option 4 selected")
+        elif choice == "5":
+            print("Option 5 selected")
+        elif choice == "6":
+            print("Option 6 selected")
+        elif choice.lower() == "x":
+            print("Exiting application...")
+            break
+        else:
+            print("Invalid choice")
+
+        print()
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils/menu.py
@@ -0,0 +1,12 @@
+def print_menu():
+    print("Conference Management")
+    print("---------------------")
+    print("MENU")
+    print("====")
+    print("1 - View Speakers & Sessions")
+    print("2 - View Attendees by Company")
+    print("3 - Add New Attendee")
+    print("4 - View Connected Attendees")
+    print("5 - Add Attendee Connection")
+    print("6 - View Rooms")
+    print("x - Exit application")
